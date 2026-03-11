fc0dee8a20daf9132042a5f7a855f2c18292656a
anhafathima5765-ai <anhafathima5765@gmail.com>
2026-03-11 14:33:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,5 +63,6 @@ if __name__ == "__main__":
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
+
